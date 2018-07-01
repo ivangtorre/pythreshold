c39f48bf48d9f275c4929c1fbfd10098c663ddd8
ivangtorre <ivangonzaleztorre@gmail.com>
2018-07-01 23:57:46 +0200

    change name

--- tests/test.py
+++ tests/test.py
@@ -7,10 +7,4 @@ Created on Fri Jun 29 21:57:09 2018
 """
 
 from pythreshold_method.thresholds import time_series
-
-def test_fahrToKelv():
-    '''
-    make sure freezing is calculated correctly
-    '''
-    
-    assert somePython.fahrToKelv(32) == 273.15, 'incorrect freezing point!'
+time_series("data/data1.txt")
